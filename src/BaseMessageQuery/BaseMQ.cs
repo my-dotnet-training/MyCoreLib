93897dd90f399ca1f80f1c82babb940631f0ee75
bitterzh <zjl9494@hotmail.com>
2018-04-10 14:11:44 +0800

    Update BaseMQ.cs

--- src/BaseMessageQuery/BaseMQ.cs
+++ src/BaseMessageQuery/BaseMQ.cs
@@ -1,11 +1,43 @@
+
+using System.Threading;
+
 namespace MyCoreLib.BaseMessageQuery
 {
     public interface IBaseMQ
     {
         void InitMQ();
-        void Product();
+        void Receive();
 
-        void Consumer();
+        void Send(string message);
 
+    }
+
+    public class BaseSingleton<T> where T : class, new()
+    {
+        protected static readonly object lockHelper = new object();
+        private static T _Instance;
+        /// <summary>
+        /// 获取单例
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    //lock (lockHelper)
+                    //{
+                    //if (_Instance == null)
+                    Interlocked.CompareExchange(ref _Instance, new T(), null);
+                }
+
+                return _Instance;
+            }
+        }
+
+        public static void Refresh()
+        {
+            _Instance = null;
+        }
     }
 }
